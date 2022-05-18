717a769f60ca7eed0fb3fe517e84af493732e0a6
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-05-18 17:20:31 -0300

    iniciando projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+	return (
+		<>
+			<Head>
+				<meta
+					name="description"
+					content="Oficina DownCars especializada em manutenção de carros."
+				/>
+				<title>DownCars</title>
+			</Head>
+
+			<main>Ola</main>
+		</>
+	);
+};
+
+export default Home;
