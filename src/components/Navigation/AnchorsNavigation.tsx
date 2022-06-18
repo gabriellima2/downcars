b395f941d0ba6c66f15e74af9f370cd631392a06
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-06-18 16:55:33 -0300

    refactor: transições mais fluídas

--- src/components/Navigation/AnchorsNavigation.tsx
+++ src/components/Navigation/AnchorsNavigation.tsx
@@ -19,7 +19,7 @@ export const AnchorsNavigation = () => {
 						<a
 							onClick={handleAnchorClick}
 							href={anchor.href}
-							className="rounded-md text-base md:text-sm tracking-widest font-medium uppercase py-3 px-5 relative after:bg-gradient-to-r after:from-transparent after:to-blue-900/70 after:rounded after:w-0 after:h-full after:absolute after:bottom-0 after:left-0 hover:after:w-full hover:after:transition-all"
+							className="rounded-md text-base md:text-sm tracking-widest font-medium uppercase py-3 px-5 relative after:bg-gradient-to-r after:from-transparent after:to-blue-900/70 after:rounded after:w-0 after:h-full after:absolute after:bottom-0 after:left-0 hover:after:w-full after:transition-all"
 						>
 							{anchor.title}
 						</a>
