cf71ddf53e1e99b0d9e44cccc291c99e97a42d42
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-06-18 22:36:29 -0300

    feat: adicionado estilo mais agradável ao footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,40 +5,39 @@ import { BackToTopButton } from "./Buttons/BackToTopButton";
 const CURRENT_YEAR = new Date().getFullYear();
 
 export const Footer = () => (
-	<footer className="flex flex-col items-center px-6 pt-6 pb-5 text-gray-100/40 bg-black border--detail">
-		<div className="w-full flex justify-between pb-3 gap-4 flex-wrap">
-			<section className="flex flex-col items-start">
-				<Image src="/logo.svg" width="160px" height="40px" />
-				<small className="capitalize text-gray-100/40">
+	<footer className="flex flex-col items-center pt-6 text-gray-100/40 bg-[#141414] border--detail">
+		<section className="w-full flex items-center justify-between pb-3 px-6 sm:px-16 gap-4 flex-wrap">
+			<div className="flex flex-col items-start">
+				<Image src="/logo.svg" width="130px" height="40px" />
+				<small className="capitalize text-gray-100/40 -mt-2 text-xs sm:text-sm">
 					oficina mecânica DownCars - {CURRENT_YEAR}
 				</small>
-			</section>
+			</div>
 			<BackToTopButton />
-		</div>
-		<section className="w-full flex flex-col items-center gap-2 text-xs pt-3 border--detail">
-			<p>
+		</section>
+		<section className="w-full flex flex-row justify-between items-center gap-1 bg-black py-2 px-6 sm:px-16 text-xs">
+			<small className="font-medium">
+				Feito por{" "}
+				<a
+					title="GitHub de Gabriel"
+					target="_blank"
+					href="https://github.com/gabriellima2"
+					className="text-gray-100/40 hover:underline"
+				>
+					Gabriel
+				</a>
+			</small>
+			<small>
 				Imagem de{" "}
 				<a
 					title="Pexels Perfil de Albin Berlin"
 					target="_blank"
 					href="https://www.pexels.com/pt-br/@albinberlin/"
+					className="hover:underline"
 				>
 					<strong>Albin Berlin</strong>
 				</a>
-			</p>
-			<p>
-				<strong>
-					Feito por{" "}
-					<a
-						title="GitHub de Gabriel"
-						target="_blank"
-						href="https://github.com/gabriellima2"
-						className="text-gray-100/40"
-					>
-						Gabriel
-					</a>
-				</strong>
-			</p>
+			</small>
 		</section>
 	</footer>
 );
