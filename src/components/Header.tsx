f798062ec37a8cdabde65ac64970917828d4a9e8
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-06-18 23:07:06 -0300

    fix: arrumado parte do header aparecendo na rolagem da página

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export const Header = ({ activateMobileNav }: HeaderProps) => {
 	return (
 		<header
 			className={`${
-				scrollDirection === "down" ? "top-0" : "-top-24"
+				scrollDirection === "down" ? "top-0" : "-top-44"
 			} bg-[#070707f3] sticky top-0 z-50 w-full h-25 transition-all duration-500 flex justify-between items-center px-10 py-5 after:w-full after:h-[1px] after:bg-gray-200-transparent after:absolute after:bottom-0 after:left-0`}
 		>
 			<a href="#home" className="flex justify-center items-center">
