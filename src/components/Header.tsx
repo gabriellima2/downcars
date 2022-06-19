15c19a90ca22681a33ec390fbe43971d1ef71d77
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-06-18 22:47:20 -0300

    fix: arrumado itens sobrepondo o header e whatsapp mobile não aparecia

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ export const Header = ({ activateMobileNav }: HeaderProps) => {
 		<header
 			className={`${
 				scrollDirection === "down" ? "top-0" : "-top-24"
-			} bg-[#070707] sticky top-0 w-full h-25 transition-all duration-500 flex justify-between items-center px-10 py-5 after:w-full after:h-[1px] after:bg-gray-200-transparent after:absolute after:bottom-0 after:left-0`}
+			} bg-[#070707f3] sticky top-0 z-50 w-full h-25 transition-all duration-500 flex justify-between items-center px-10 py-5 after:w-full after:h-[1px] after:bg-gray-200-transparent after:absolute after:bottom-0 after:left-0`}
 		>
 			<a href="#home" className="flex justify-center items-center">
 				<Image
@@ -28,14 +28,15 @@ export const Header = ({ activateMobileNav }: HeaderProps) => {
 			</a>
 			<div className="hidden md:flex md:gap-5">
 				<AnchorsNavigation />
-				{isScrolling ? (
-					<span className="flex fixed bottom-8 right-8">
-						<WhatsappButton />
-					</span>
-				) : (
-					<WhatsappButton />
-				)}
+
+				{!isScrolling && <WhatsappButton />}
 			</div>
+
+			{isScrolling && (
+				<span className="flex fixed bottom-8 right-8">
+					<WhatsappButton />
+				</span>
+			)}
 
 			<button className="md:hidden" onClick={activateMobileNav}>
 				<Image src="/menu.svg" width="40px" height="40px" />
